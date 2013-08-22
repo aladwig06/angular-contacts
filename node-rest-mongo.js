c23c460ea87f0e2b63da0f517bf26594e0e477bf
Andrew Ladwig <webmaster@texasfishingconnection.com>
2013-08-22 13:43:33 -0500

    formatting changes to new contact form

--- node-rest-mongo.js
+++ node-rest-mongo.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
